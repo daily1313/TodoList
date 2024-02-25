2811d554a85924187c5e727ace38cec6680965b6
daily1313 <kimsb7218@naver.com>
2024-02-25 18:13:25 +0900

    chore: Separate item, title directives

--- js/script.js
+++ js/script.js
@@ -56,25 +56,3 @@ app.controller('TodoCtrl', function ($scope) {
         $scope.newTodoTitle = "";
     }
 });
-
-app.directive('todoTitle', function() {
-    return {
-        template: '<h1>TodoList</h1>'
-    }
-}) 
-
-app.directive('todoItem', function() {
-    return {
-        template: 
-            '<div class="input-group">' +
-                '<span class="input-group-addon">' +
-                    '<input type="checkbox" ng-model="todo.completed">' +
-                '</span>' +
-                '<input type="text" class="form-control" ng-model="todo.title">' +
-                '<span class="input-group-btn">' +
-                    '<button class="btn btn-danger" type="button" ng-click="remove(todo)">삭제</button>' +
-                '</span>' +
-            '</div>' +
-            '<date>{{todo.createdAt | date: "yyyy-MM-dd HH:mm:ss" }}</date>'
-    }
-})
