6fbc1fafe42d48dff5b5ae35043bf6204fbf4f9f
daily1313 <kimsb7218@naver.com>
2024-02-25 21:21:21 +0900

    feat: Apply local storage to store data

--- js/service.js
+++ js/service.js
@@ -1,4 +1,5 @@
 angular.module('todo').factory('todoStorage', function() {
+    var TODO_DATA = 'TODO_DATA';
     var storage = {
         todos: [
             {
@@ -27,8 +28,22 @@ angular.module('todo').factory('todoStorage', function() {
                 createdAt: Date.now()
             }
         ],
+        
+        // _ : private: Use From Controller (x) 
+        // JSON.stringify(data) 한 값을 localStorage에 저장
+
+        _saveToLocalStorage: function (data) {
+            localStorage.setItem(TODO_DATA, JSON.stringify(data))
+        },
+
+        // String 값을 JSON으로 변경하여 LocalStorage에서 데이터를 가져옴
+
+        _getFromLocalStorage: function () {
+            return JSON.parse(localStorage.getItem(TODO_DATA)) || [];
+        },
 
         get: function() {
+            angular.copy(storage._getFromLocalStorage(), storage.todos)
             return storage.todos;
         },
 
@@ -52,6 +67,7 @@ angular.module('todo').factory('todoStorage', function() {
             };
     
             storage.todos.push(newTodo);
+            storage._saveToLocalStorage(storage.todos)
         }
     }
     return storage;
